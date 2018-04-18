58ffe69929f4336bde7b4ad6983d6620a41ae4de
root <root@ip-172-31-25-72.ap-south-1.compute.internal>
2018-04-18 09:41:09 +0000

    gg

--- repository.groovy
+++ repository.groovy
@@ -2,9 +2,12 @@
 
 def callS3Upload(mesg){
   stage("${mesg}") {
-    withAWS(credentials:'mobile-s3-user',region:'ap-south-1') {
-	s3Upload(acl:'PublicRead',bucket:'mybucket-ssp',cacheControl:'',excludePathPattern:'',file:"Build-${env.BUILD_NUMBER}",metadatas:[''],path:"${env.BRANCH_NAME}/Build-${env.BUILD_NUMBER}")
-    }
+//    withAWS(credentials:'mobile-s3-user',region:'ap-south-1') {
+//	s3Upload(acl:'PublicRead',bucket:'mybucket-ssp',cacheControl:'',excludePathPattern:'',file:"Build-${env.BUILD_NUMBER}",metadatas:[''],path:"${env.BRANCH_NAME}/Build-${env.BUILD_NUMBER}")
+//    }
+withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'mobile-s3-user', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+s3Upload(acl:'PublicRead',bucket:'mybucket-ssp',cacheControl:'',excludePathPattern:'',file:"Build-${env.BUILD_NUMBER}",metadatas:[''],path:"${env.BRANCH_NAME}/Build-${env.BUILD_NUMBER}")
+}
   }
 }
 
