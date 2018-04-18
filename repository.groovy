1e10f087db54db381e8d0ed37f2750589e68a8f9
root <root@ip-172-31-25-72.ap-south-1.compute.internal>
2018-04-18 10:57:10 +0000

    jj

--- repository.groovy
+++ repository.groovy
@@ -13,11 +13,8 @@ withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariab
 //s3Upload(acl:'PublicRead',bucket:'mybucket-ssp',cacheControl:'',excludePathPattern:'',file:"Build-${env.BUILD_NUMBER}",metadatas:[''],path:"${env.BRANCH_NAME}/Build-${env.BUILD_NUMBER}")
 s3Upload consoleLogLevel: 'INFO', dontWaitForConcurrentBuildCompletion: false, entries: [[bucket: 'mybucket-ssp', excludedFile: '', flatten: false, gzipFiles: false, keepForever: false, managedArtifacts: false, noUploadOnFailure: false, selectedRegion: 'ap-south-1', showDirectlyInBrowser: false, file:"Build-${env.BUILD_NUMBER}", path:"${env.BRANCH_NAME}/Build-${env.BUILD_NUMBER}" , storageClass: 'STANDARD', uploadFromSlave: false, useServerSideEncryption: false]], pluginFailureResultConstraint: 'FAILURE', profileName: 'mobileapp', userMetadata: []
 }
-
-
 }
   }
-}
 
 def artifactory(){
   callS3Upload("Upload to S3")
